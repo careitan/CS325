104329140d60771838d444789fbd8b388a3dd89f
Craig Reitan <reitanc@flip1.engr.oregonstate.edu>
2018-02-18 13:41:11 -0800

    initial build and upload of files

---
+++ HW5/HW5.cpp
@@ -0,0 +1,45 @@
+// CS 325 - W2018 Homework 5
+// Oregon State University
+// (Craig) Allan Reitan
+// 
+// HW5.cpp - BFS & DFS for Graph Edge determinations.
+// 
+// useage: HW5 file_name
+// 
+// Purpose: Read in a set of activites identified in a text file.
+// 		format of the input text file is using integers and assumed to be the following:
+//		Total_items_in_Set
+//		Number0 Start0 Finish0
+//		...
+//		NumberN-1 StartN-1 FinishN-1
+//		
+
+#include <stdio.h>
+#include <algorithm>
+#include <cstring>
+#include <math.h>
+#include <vector>
+#include <deque>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <stdexcept>
+#include <iterator>
+#include <climits>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	if (argc <=1)
+	{
+		printf("ERROR: no file name provided.\nUSAGE: HW5 file_name\n");
+	}
+
+	return 0;
+}
+
+//////////////////////////
+//	FUNCTION BUILDS
+//////////////////////////
